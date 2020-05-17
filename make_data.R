1a8912cd55245b70ffae3a4d6fd7120de8aca3cb
haututu <35088807+haututu@users.noreply.github.com>
2020-05-17 12:16:35 +1200

    Fear of covid validation built off some old code.

---
+++ make_data.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(lavaan)
+library(haven)
+library(brms)
+
+covid_dat <- readxl::read_xlsx("data/L4SurveyCoded.xlsx") %>%
+  select(ResponseId, Gender, Age, starts_with("FofCOVID")) %>%
+  mutate(Gender = case_when(
+    Gender == 1 ~ "male", 
+    Gender == 2 ~ "female"
+  )
+  ) %>%
+  na.omit()
+
+#sapply(covid_dat, function(x) attributes(x)$label) %>% 
+#  data.frame(var = names(.), description = .) %>% 
+#  mutate(description = gsub("\n", "", description)) %>%
+#  mutate(description = gsub("Please indicate how much you agree with each of the following statements. Respond withhow you feel right now. - ", "", description)) %>%
+#  mutate(description = gsub("Please indicatehow much you agree with each of the following statements. Respond withhow you feel right now.", "", description)) %>%
+#  write_csv("question_labels.csv")
+
+covid_fear <- cfa(
+  paste("fear =~", paste0(covid_cols[grepl("FofCOVID", covid_cols)], collapse = " + ")),
+  missing = "ML",
+  data = covid_dat
+)
+
+covid_dat <- bind_cols(
+  select(covid_dat, ResponseId, Age, Gender, Political_Beliefs, National, Labour), 
+  data.frame(predict(covid_authority)),
+  data.frame(predict(covid_fear))
+) %>%
+  mutate_if(is.numeric, function(x) {as.numeric(scale(x))})
