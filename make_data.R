f0f10d6276e792e7379803675b28295fc4c1f8db
haututu <35088807+haututu@users.noreply.github.com>
2020-05-17 21:49:44 +1200

    Probably broke it by removing na.omit() on load.

--- make_data.R
+++ make_data.R
@@ -6,8 +6,8 @@ library(eRm)
 
 # Load data
 covid_dat <- bind_rows(
-  read_sav("data/L3Clean.sav") %>% select(Gender, Age, Ethnicity, starts_with("FofCOVID_"), starts_with("PVD_")),
-  read_sav("data/L4Clean.sav") %>% select(Gender, Age, Ethnicity, starts_with("FofCOVID_"), starts_with("PVD_"))
+  read_sav("data/L3Clean.sav") %>% select(Gender, Age, Ethnicity, starts_with("FofCOVID_"), starts_with("PVD_"), starts_with("WEMWBS")),
+  read_sav("data/L4Clean.sav") %>% select(Gender, Age, Ethnicity, starts_with("FofCOVID_"), starts_with("PVD_"), starts_with("WEMWBS"))
   ) %>%
   mutate(Gender = case_when(
     Gender == 1 ~ "male", 
@@ -20,8 +20,7 @@ covid_dat <- bind_rows(
     Ethnicity == 4 ~ "asian",
     TRUE ~ "other"
   )
-  ) %>%
-  na.omit()
+  )
 
 # Demographic information
 
@@ -36,27 +35,57 @@ covid_dat %>%
             ) %>%
   gather(key, value)
 
+# Fear of covid ############################################
 covid_cols <- colnames(covid_dat)[grepl("FofCOVID", colnames(covid_dat))]
 
-# CFA
 covid_fear <- cfa(
   paste("fear =~", paste0(covid_cols, collapse = " + ")),
-  missing = "ML",
   data = covid_dat
 )
 
 summary(covid_fear, fit=TRUE, standardized=TRUE)
 
+# PVD ##################################################
+infect_cols <- colnames(covid_dat)[grepl("PVD_", colnames(covid_dat)) & grepl("_[2568]|1[024]", colnames(covid_dat))]
+germ_cols <- colnames(covid_dat)[grepl("PVD_", colnames(covid_dat)) & !grepl("_[2568]|1[024]", colnames(covid_dat))]
+
+covid_pvd <- cfa(
+  paste(
+    paste("infect =~", paste0(infect_cols, collapse = " + ")),
+    paste("germ =~", paste0(germ_cols, collapse = " + ")),
+    sep="\n"
+  ),
+  data = covid_dat
+)
+
+# Mental wellbeing ############################################
+covid_cols <- colnames(covid_dat)[grepl("WEMWBS", colnames(covid_dat))]
+
+covid_mwb <- cfa(
+  paste("MWB =~", paste0(covid_cols, collapse = " + ")),
+  data = covid_dat
+)
+
+summary(covid_fear, fit=TRUE, standardized=TRUE)
+
+summary(covid_pvd, fit=TRUE, standardized=TRUE)
+
+summary(covid_mwb, fit=TRUE, standardized=TRUE)
+
+######################################### Comparisons ################################################
+
+# Infect to fear
+cor.test(predict(covid_pvd)[,1], predict(covid_fear))
+
+# Germ to fear
+cor.test(predict(covid_pvd)[,2], predict(covid_fear))
+
+# MWB to fear
+cor.test(predict(covid_mwb), predict(covid_fear))
+
 # Cronback alpha
 ltm::cronbach.alpha(covid_dat %>% select(covid_cols), standardized = TRUE)
+ltm::cronbach.alpha(covid_dat %>% select(pvd_cols), standardized = TRUE)
+ltm::cronbach.alpha(covid_dat %>% select(covid_cols), standardized = TRUE)
 
-# Rasch model
-rsm_results <- RSM(covid_dat %>% select(covid_cols))
-itemfit(covid_dat %>% select(covid_cols))
 
-covid_dat <- bind_cols(
-  select(covid_dat, ResponseId, Age, Gender, Political_Beliefs, National, Labour), 
-  data.frame(predict(covid_authority)),
-  data.frame(predict(covid_fear))
-) %>%
-  mutate_if(is.numeric, function(x) {as.numeric(scale(x))})
